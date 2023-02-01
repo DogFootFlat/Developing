75dd32c8ef2723ad775b0a44f4375413669de706
Jihee Eom <65202243+bonjenny@users.noreply.github.com>
2023-02-02 02:23:55 +0900

    커맨드 추가

--- front-end/src/components/route/RouterComponent.jsx
+++ front-end/src/components/route/RouterComponent.jsx
@@ -1,27 +1,31 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Routes, Route } from "react-router-dom";
+import { CircularProgress } from "@mui/material";
+
 import Home from "../home/Home";
-import BookList from "../book/BookList";
-import AddBook from "../book/AddBook";
-import EditBook from "../book/EditBook";
-import UserHome from "../home/UserHome";
-import UserList from "../user/UserList";
-import AddUser from "../user/AddUser";
-import EditUser from "../user/EditUser";
+const UserHome = React.lazy(() => import("../home/UserHome"))
+const BookList = React.lazy(() => import("../book/BookList"))
+const AddBook = React.lazy(() => import("../book/AddBook"))
+const EditBook = React.lazy(() => import("../book/EditBook"))
+const UserList = React.lazy(() => import("../user/UserList"))
+const AddUser = React.lazy(() => import("../user/AddUser"))
+const EditUser = React.lazy(() => import("../user/EditUser"))
 
 const AppRouter = () => {
   return (
     <div style={style}>
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/user-home" element={<UserHome />} />
-        <Route path="/books" element={<BookList />} />
-        <Route path="/add-book" element={<AddBook />} />
-        <Route path="/edit-book" element={<EditBook />} />
-        <Route path="/users" element={<UserList />} />
-        <Route path="/add-user" element={<AddUser />} />
-        <Route path="/edit-user" element={<EditUser />} />
-      </Routes>
+      <Suspense fallback={ <CircularProgress /> }>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/user-home" element={<UserHome />} />
+          <Route path="/books" element={<BookList />} />
+          <Route path="/add-book" element={<AddBook />} />
+          <Route path="/edit-book" element={<EditBook />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="/edit-user" element={<EditUser />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
