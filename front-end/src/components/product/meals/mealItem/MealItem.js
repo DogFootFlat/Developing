abb62d6562f3145bb2342170d1ff36a826cde4c9
Jihee Eom <bonjenny1@gmail.com>
2024-05-25 14:00:00 +0900

    5/25 - 상품목록 연결 완료

--- front-end/src/components/product/meals/mealItem/MealItem.js
+++ front-end/src/components/product/meals/mealItem/MealItem.js
@@ -6,7 +6,7 @@ import classes from "./css/MealItem.module.css";
 
 const MealItem = (props) => {
   const cartCtx = useContext(CartContext);
-  const price = `$${props.price.toFixed(2)}`;
+  const price = `${props.price} 원`;
 
   const addToCartHandler = (amount) => {
     cartCtx.addItem({
@@ -21,7 +21,8 @@ const MealItem = (props) => {
     <li className={classes.meal}>
       <div>
         <h3>{props.name}</h3>
-        <div className={classes.description}>{props.description}</div>
+        <div className={classes.description}>{props.genre}</div>
+        <div><img src={props.img}></img></div>
         <div className={classes.price}>{price}</div>
       </div>
       <div>
