f589fcd87f4c275682a2694173e2213600af2341
Jihee Eom <bonjenny1@gmail.com>
2024-09-25 00:41:59 +0900

    fix Select width

--- src/components/product/products/productForm/ProductFilter.js
+++ src/components/product/products/productForm/ProductFilter.js
@@ -1,23 +1,28 @@
 import { FormControl, InputLabel, MenuItem, Select } from '@material-ui/core';
-import React, { useState } from 'react';
+import React from 'react';
+import classes from './css/ProductFilter.module.css';
 
 const ProductFilter = ({ props: { filter_name, filter, setFilter, onChange, filter_list } }) => {
-	const onChangeHandler = (event) => {
-		setFilter(event.target.value);
-		onChange();
-	};
+  const onChangeHandler = (event) => {
+    setFilter(event.target.value);
+    onChange();
+  };
 
-	return (
-		<FormControl variant="filled" fullWidth>
-			<InputLabel htmlFor="uncontrolled-native">{filter_name}</InputLabel>
-			<Select displayEmpty id={filter_name} value={filter} onChange={onChangeHandler} sx={{ textAlign: 'right' }}>
-				<MenuItem value="">전체</MenuItem>
-				{filter_list?.map((menu_item) => (
-					<MenuItem value={menu_item.value}>{menu_item.label}</MenuItem>
-				))}
-			</Select>
-		</FormControl>
-	);
+  return (
+    <span className={classes['form-div']}>
+      <FormControl variant="outlined" className={classes['form-control']}>
+        <InputLabel htmlFor="uncontrolled-native">{filter_name}</InputLabel>
+        <Select displayEmpty id={filter_name} value={filter} onChange={onChangeHandler} sx={{ textAlign: 'right' }}>
+          <MenuItem value="">전체</MenuItem>
+          {filter_list?.map((menu_item) => (
+            <MenuItem key={menu_item.value} value={menu_item.value}>
+              {menu_item.label}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </span>
+  );
 };
 
 export default ProductFilter;
