f777791ec42b5f92b916647babc47d35455f3e51
shrt0123 <shrt0123@naver.com>
2024-10-12 13:35:52 +0900

    챗봇 css 및 로드 수정

--- src/components/basic/SideBar.jsx
+++ src/components/basic/SideBar.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './css/SideBar.module.css';
 
 const Sidebar = ({ isVisible, content, onClose }) => {
+  const [key, setKey] = useState(0);
+
+  const handleOnClose = () => {
+    setKey(prevKey => prevKey + 1); // 키를 변경하여 컴포넌트를 다시 마운트
+    onClose(); // 부모 컴포넌트의 onClose 함수 호출하여 사이드바 닫기
+  };
+
   return (
     <div className={`${classes.sidebar} ${isVisible ? `${classes.visible}` : ''}`}>
       <div className={classes['sidebar-button']}>
-        <button onClick={onClose}>
-        X
-      </button>
+        <button onClick={handleOnClose}>X</button>
       </div>
-      {content}
+      <div key={key}>{content}</div> {/* 키가 변경될 때마다 콘텐츠가 다시 마운트됨 */}
     </div>
   );
 };
